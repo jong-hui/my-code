18444c70238b2120f08ee9bbc0502afc7fd74628
jong-hui <webd200@gmail.com>
2020-11-06 14:28:22 +0900

    오브젝트 리플레이서 추가

---
+++ js/오브젝트 리플레이서/index.js
@@ -0,0 +1,12 @@
+const _ = require('lodash')
+
+exports.default = {
+  stringReplacer: function (templateString, object) {
+    const replacerRegex = new RegExp(/\{\{(.+)\}\}/, 'g')
+
+    templateString.replace(replacerRegex, (match, p1) => {
+      const currentReplaceString = _.get(object, p1)
+      return currentReplaceString === undefined ? p1 : currentReplaceString
+    })
+  }
+}
