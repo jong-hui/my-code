c35175d1349bc169f261841b43d7c00c179a2333
jong_hui <42797995+jong-hui@users.noreply.github.com>
2021-02-17 11:29:08 +0900

    add DeepPartial

--- ts/utilTypes.ts
+++ ts/utilTypes.ts
@@ -1,6 +1,10 @@
 export type SomePartial<T, K extends keyof T> = Partial<Pick<T, K>> & Omit<T, K>
 
-  export type AsyncReturnType<T extends (...args: any) => any> =
+export type AsyncReturnType<T extends (...args: any) => any> =
   T extends (...args: any) => Promise<infer U> ? U :
   T extends (...args: any) => infer U ? U :
   any
+
+export type DeepPartial<T> = {
+  [P in keyof T]?: DeepPartial<T[P]>;
+};
